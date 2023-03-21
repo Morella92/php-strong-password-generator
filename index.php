fcb55614ee7ffe2f014af88627294d0241b41a0e
Morella92 <morena.piem@outlook.com>
2023-03-21 16:40:40 +0100

    FEAT function for random password

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
     <title>Password generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-primary">
     
     <div class="container py-5">
 
-        <div class="row">
-            <div class="text-center col-12">
+        <div class="row text-center text-white">
+            <div class="col-12">
                 <h1>
                     Strong password Generator
                 </h1>
@@ -20,17 +20,33 @@
                     Genera una password sicura
                 </h2>
             </div>
+
+            <div class="col-12">
+                <!-- Creo un form -->
+                <form action="" method="GET" class="py-3">
+                    <label for="length">Lunghezza Password:</label>
+                    <input type="number" name="length" min="8" max="16">
+                    <input type="submit" value="Genera Password">
+                </form>
+            </div>
         </div>
         
-        <div class="col-12">
-            <!-- Creo un form -->
-            <form action="" method="GET" class="text-center py-3">
-                <label for="length">Lunghezza Password:</label>
-                <input type="number" name="length" min="8" max="16">
-                <input type="submit" value="Genera Password">
-            </form>
-        </div>
     </div>
     
+    <?php 
+    
+        // Funzione per generare una password casuale
+        function generate_password($length) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&*()_-,.?";
+            $password = "";
+        
+            for ($i = 0; $i < $length; $i++) {
+                $random_password = random_int(0, strlen($chars) - 1);
+                
+            }
+        
+        }
+    
+    ?>
 </body>
 </html>
